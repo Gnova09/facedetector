1579a1bcddc6374a10a4643a58281326450a5583
Gnova09 <Georges.bueno@hotmail.com>
2022-06-28 15:52:13 -0400

    done
    Se cambio funtionc App a una Class app extends

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import React, { useCallback } from 'react';
+import React, { Component } from 'react';
 import Navigator from "./Components/Navigator/Navidator";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
@@ -9,22 +9,33 @@ import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
 import Rank from './Components/Rank/Rank';
 
-function App() {
 
-  const particlesInit = useCallback(main => {
-    loadFull(main);
-  }, []);
+//aqui es donde se inicilizan las particulas
+const particlesInit = async (main) => { 
+  console.log(main);
+  await loadFull(main);
+};
 
-  return (
-    <div className="App">
-      <Particles options={Particlesconfig} init={particlesInit}/>
-      <Navigator/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
-      {/*<FaceRecognition/>*/}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+    }
+  }
+ 
+  render(){
+    return (
+      <div className="App">
+        <Particles options={Particlesconfig} init={particlesInit}/>
+        <Navigator/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm/>
+        {/*<FaceRecognition/>*/}
+      </div>
+    );
+  }  
 }
 
 export default App;
